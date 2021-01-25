43c68d0c4e250071f8fe94fb813d7576daf6cce8
Alberto Basalo <albertobasalo@hotmail.com>
2021-01-25 10:58:24 +0100

    feat: do route based logic on navigation ends - Close #13

--- apps/showcase/src/app/core/core-routing.module.ts
+++ apps/showcase/src/app/core/core-routing.module.ts
@@ -1,16 +1,33 @@
+import { HeadService } from '@angular.builders/ui';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterModule,
+  Routes,
+} from '@angular/router';
 
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () =>
       import('@angular.builders/auth').then((module) => module.AuthModule),
+    data: {
+      pageTitle: 'Authorization',
+      pageDescription: 'Autentication and authorization pages',
+      hideHeader: true,
+    },
   },
   {
     path: '',
     loadChildren: () =>
       import('../routes/home/home.module').then((m) => m.HomeModule),
+    data: {
+      pageTitle: 'Gallery',
+      pageDescription: 'The gallery of resources for Angular developers',
+      hideHeader: true,
+    },
   },
 ];
 
@@ -18,4 +35,23 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class CoreRoutingModule {}
+export class CoreRoutingModule {
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private headService: HeadService
+  ) {
+    this.router.events.subscribe((routerEvent) => {
+      console.log(routerEvent);
+      if (routerEvent instanceof NavigationEnd) {
+        console.warn('Do something with new URL ' + routerEvent.url);
+        const routeData = this.activatedRoute.firstChild?.snapshot.data;
+        if (routeData) {
+          this.headService.setTitle(routeData.pageTitle || '');
+          this.headService.setDescription(routeData.pageDescription || '');
+        }
+        // ToDo: send signal to show or hide header/footer
+      }
+    });
+  }
+}
